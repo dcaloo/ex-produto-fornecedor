6d743a6898ce8b21fa37cfce6cf55c7fff4c33e0
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-08 08:53:04 -0300

    exercicio completo

---
+++ src/Util.java
@@ -0,0 +1,95 @@
+import java.text.DecimalFormat;
+
+import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.parseInt;
+import static java.lang.Double.parseDouble;
+
+public class Util {
+    private Produto[] produto = new Produto[5];
+    private Fornecedor[] fornecedor = new Fornecedor[5];
+    private int idxProduto = 0;
+    private int idxFornecedor = 0;
+
+    public void menu() {
+        int opcao;
+        String msg = "1. Cadastrar Produto \n2. Pesquisar produto" +
+                     "\n3. Pesquisar fornecedor\n4. Finalzar";
+
+        while(true) {
+            opcao = parseInt(showInputDialog(msg));
+            if(opcao == 4) {
+                return;
+            }
+            switch (opcao) {
+                case 1:
+                    cadastrarProduto();
+                    break;
+                case 2:
+                    pesquisarProduto();
+                    break;
+                case 3:
+                    pesquisarFornecedor();
+                    break;
+                default:
+                    showMessageDialog(null,"Opção Inválida!");
+            }
+        }
+
+    }
+
+    private void cadastrarProduto() {
+        String nome;
+        double valor;
+        int qtdEstoque;
+        Fornecedor fornecedor = pesquisarFornecedor();
+
+        if(fornecedor == null) {
+            fornecedor = cadastrarFornecedor();
+        }
+
+        nome = showInputDialog("Nome do produto");
+        valor = parseDouble(showInputDialog("Valor unitário"));
+        qtdEstoque = parseInt(showInputDialog("Quantidade em estoque"));
+        produto[idxProduto] = new Produto(nome, valor, qtdEstoque, fornecedor);
+        idxProduto++;
+    }
+
+    private Produto pesquisarProduto() {
+        DecimalFormat df = new DecimalFormat("0.00");
+        String aux = "Produto não encontrado";
+        String nome = showInputDialog("Nome do Produto");
+        for(int i = 0; i < idxProduto; i++) {
+            if(produto[i].getNome().equalsIgnoreCase(nome)) {
+                aux = "";
+                aux += "Nome do Produto: " + nome + "\n";
+                aux += "Valor unitário: R$ " + df.format(produto[i].getValor()) + "\n";
+                aux += "Nome do Fornecedor: " + produto[i].getFornecedor().getNome() + "\n";
+                aux += "Quantidade em estoque: " + produto[i].getQtdEstoque() + "\n";
+            }
+        }
+        showMessageDialog(null, aux);
+        return null;
+    }
+
+    private Fornecedor pesquisarFornecedor(){
+        int cnpj = parseInt(showInputDialog("CNPJ do fornecedor"));
+        for(int i = 0; i < idxFornecedor; i++) {
+            if(fornecedor[i].getCnpj() == cnpj){
+                return fornecedor[i];
+            }
+        }
+        showMessageDialog(null, cnpj + " não cadastrado");
+        return null;
+    }
+    private Fornecedor cadastrarFornecedor() {
+        String nome;
+        int cnpj;
+
+        nome = showInputDialog("Nome do Fornecedor");
+        cnpj = parseInt(showInputDialog("CNPJ do Fornecedor"));
+
+        fornecedor[idxFornecedor] = new Fornecedor(nome, cnpj);
+        idxFornecedor++;
+        return fornecedor[idxFornecedor - 1];
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,12 @@
+import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.parseInt;
+
+public class main {
+    public static void main(String[] args) {
+        //Util util = new Util();
+        //util.menu();
+
+        new Util().menu();
+
+    }
+}
